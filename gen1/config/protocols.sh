0ee8ef9d20d4bc2c473ab827a00b9d20ecabff22
Hekatos Noir <soulwhisper@outlook.com>
2025-04-01 06:39:58 +0000

    feat(prot): add k8s bfd support

--- gen1/config/protocols.sh
+++ gen1/config/protocols.sh
@@ -1,6 +1,6 @@
 #!/bin/vbash
 
-# BGP
+# BGP; TCP 179
 set protocols bgp system-as 65510
 set protocols bgp parameters router-id '10.10.0.1'
 set protocols bgp address-family ipv4-unicast aggregate-address 10.10.0.0/24
@@ -14,3 +14,9 @@ set protocols bgp peer-group k8s address-family ipv4-unicast soft-reconfiguratio
 set protocols bgp neighbor 10.10.0.101 peer-group k8s
 set protocols bgp neighbor 10.10.0.102 peer-group k8s
 set protocols bgp neighbor 10.10.0.103 peer-group k8s
+
+# BFD; UDP 3784
+set protocols bgp peer-group k8s bfd
+set protocols bfd peer 10.10.0.101 interval 300 multiplier 3
+set protocols bfd peer 10.10.0.102 interval 300 multiplier 3
+set protocols bfd peer 10.10.0.103 interval 300 multiplier 3
